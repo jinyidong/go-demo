5455a0b414d5d503f3f4bc47acf7114b2cd4a3a2
jinyidong <jinyidong@outlook.com>
2019-03-12 10:22:45 +0800

    test

---
+++ tdd/tdd.go
@@ -0,0 +1,6 @@
+package tdd
+
+//先定义接口/方法签名，后写测试用例
+var Add= func(i,j int) int {
+	return i+j
+}
---
+++ tdd/tdd_test.go
@@ -0,0 +1,25 @@
+package tdd
+
+import (
+	"github.com/prashantv/gostub"
+	"github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	convey.Convey("1+1",t, func() {
+		convey.So(Add(1,2),convey.ShouldEqual,3)
+	})
+}
+
+func TestAdd2(t *testing.T) {
+	convey.Convey("1+(-1)",t, func() {
+		convey.So(Add(1,-1),convey.ShouldEqual,0)
+	})
+}
+
+//stub使用时需要将方法改写为var funName=fun()
+func TestAdd3(t *testing.T) {
+	stubs:=gostub.StubFunc(&Add,1)
+	defer stubs.Reset()
+}
